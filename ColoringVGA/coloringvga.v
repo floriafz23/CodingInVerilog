4fdd5c4f87c977b95d677b79b0523f75f1ef0d70
Floria Fang Zhang <74149949+floriafz23@users.noreply.github.com>
2022-01-06 20:20:49 -0500

    Update coloringvga.v

--- ColoringVGA/coloringvga.v
+++ ColoringVGA/coloringvga.v
@@ -1,29 +1,3 @@
-`timescale 1ns / 1ps
-`default_nettype none
-
-module main	(
-	input wire CLOCK_50,            //On Board 50 MHz
-	input wire [9:0] SW,            // On board Switches
-	input wire [3:0] KEY,           // On board push buttons
-	output wire [6:0] HEX0,         // HEX displays
-	output wire [6:0] HEX1,         
-	output wire [6:0] HEX2,         
-	output wire [6:0] HEX3,         
-	output wire [6:0] HEX4,         
-	output wire [6:0] HEX5,         
-	output wire [9:0] LEDR,         // LEDs
-	output wire [7:0] x,            // VGA pixel coordinates
-	output wire [6:0] y,
-	output wire [2:0] colour,       // VGA pixel colour (0-7)
-	output wire plot,               // Pixel drawn when this is pulsed
-	output wire vga_resetn          // VGA resets to black when this is pulsed (NOT CURRENTLY AVAILABLE)
-);    
-
-	part2 u0(KEY[0],~KEY[1],~KEY[2],SW[9:7],~KEY[3],SW[6:0],CLOCK_50,x,y,colour,plot);
-	
-endmodule
-
-
 
 //connecting the datapath and control path
 
